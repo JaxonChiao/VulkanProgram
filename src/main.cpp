91ff005c7214d7915d155993c115584f7866f82b
Jiaxiong Jiao <jiaxiongjiao@icloud.com>
2022-01-03 16:48:27 -0500

    finished basic vulkan setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
+#include <cmath>
 #include <iostream>
+#include "vulkan.h"
+#include <vector>
+
+class VulkanProgram
+{
+	public:
+		void run()
+		{
+			initVulkan();
+		}
+	private:
+		void initVulkan()
+		{
+			uint32_t layerCount = 0;
+			vkEnumerateInstanceLayerProperties(&layerCount,
+											   nullptr);
+			std::cout << layerCount << std::endl;
+
+			std::vector<VkLayerProperties> layers;
+			layers.resize(layerCount);
+
+			vkEnumerateInstanceLayerProperties(&layerCount, layers.data());
+
+			for (const auto& layer : layers)
+			{
+				std::cout << layer.layerName << std::endl;
+			}
+		}
+};
+
 
 int main()
 {
-	std::cout << "Hi" << std::endl;
+	VulkanProgram program{};
+	program.run();
 }
