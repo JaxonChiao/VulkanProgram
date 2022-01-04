5080071df5f5c7f391d10d3a629019606cab6ef9
Jiaxiong Jiao <jiaxiongjiao@icloud.com>
2022-01-04 17:19:08 -0500

    Added memory stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,79 @@
-#include <cmath>
+#include <bitset>
 #include <iostream>
 #include "vulkan.h"
 #include <vector>
+#include <cstdlib>
 
-class VulkanProgram
-{
-    public:
-        void run()
-            {
-                initVulkan();
-            }
-    private:
-        void initVulkan()
-        {
-            uint32_t layerCount = 0;
-            vkEnumerateInstanceLayerProperties(&layerCount,
-                                                nullptr);
-            std::cout << layerCount << std::endl;
+class VulkanProgram {
+public:
+    void run() {
+        initVulkan();
+    }
 
-            std::vector<VkLayerProperties> layers;
-            layers.resize(layerCount);
+private:
+    VkBool32 vkResult{};
+    struct VulkanProgramInfo {
+        VkInstance vulkanInstance = VK_NULL_HANDLE;
+        VkPhysicalDevice GPU;
+        std::vector<const char *> LayerEnabled =
+                {
+                    "VK_LAYER_KHRONOS_validation"
+                };
 
-            vkEnumerateInstanceLayerProperties(&layerCount, layers.data());
+        std::vector<const char *> InstanceExtensionsEnabled =
+                {
+                };
 
-            for (const auto& layer : layers)
-            {
-                std::cout << layer.layerName << std::endl;
-            }
+    } vulkanProgramInfo;
+
+    void initVulkan() {
+        VkInstanceCreateInfo instanceCreateInfo{};
+        instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        instanceCreateInfo.enabledExtensionCount = vulkanProgramInfo.InstanceExtensionsEnabled.size();
+        instanceCreateInfo.ppEnabledExtensionNames = vulkanProgramInfo.InstanceExtensionsEnabled.data();
+        instanceCreateInfo.enabledLayerCount = vulkanProgramInfo.LayerEnabled.size();
+        instanceCreateInfo.ppEnabledLayerNames = vulkanProgramInfo.LayerEnabled.data();
+        instanceCreateInfo.flags = 0;
+
+        vkResult = vkCreateInstance(&instanceCreateInfo,
+                                    nullptr,
+                                    &vulkanProgramInfo.vulkanInstance);
+
+        if (vkResult != VK_SUCCESS) {
+            std::cout << "Failed to create Vulkan instance" << std::endl;
+            exit(-1);
         }
-    };
+
+        uint32_t  physicalDeviceCount = 0;
+        vkEnumeratePhysicalDevices(vulkanProgramInfo.vulkanInstance,
+                                   &physicalDeviceCount,
+                                   nullptr);
+
+        std::vector<VkPhysicalDevice> GPUs(physicalDeviceCount);
+        vkEnumeratePhysicalDevices(vulkanProgramInfo.vulkanInstance,
+                                   &physicalDeviceCount,
+                                   GPUs.data());
+
+        vulkanProgramInfo.GPU = GPUs[0];
+        VkFormatProperties formatProperties;
+        vkGetPhysicalDeviceFormatProperties(vulkanProgramInfo.GPU,
+                                            VK_FORMAT_R8G8B8A8_UNORM,
+                                            &formatProperties);
+
+        VkPhysicalDeviceMemoryProperties memoryProperties{};
+        vkGetPhysicalDeviceMemoryProperties(vulkanProgramInfo.GPU, &memoryProperties);
+        std::cout << "Memory type count: " << memoryProperties.memoryTypeCount << std::endl;
+        std::cout << "Memory type heap index: " << memoryProperties.memoryTypes->heapIndex << std::endl;
+        std::cout << "Memory type property flag: " << std::bitset<32>(memoryProperties.memoryTypes->propertyFlags) << std::endl;
+
+        std::cout << "Memory heap count: " << memoryProperties.memoryHeapCount << std::endl;
+        std::cout << "Memory heap size: " << memoryProperties.memoryHeaps->size << std::endl;
+        std::cout << "Memory heap property flags: " << std::bitset<32>(memoryProperties.memoryHeaps->flags) << std::endl;
+    }
+};
 
 
-int main()
-{
+int main() {
     VulkanProgram program{};
     program.run();
 }
