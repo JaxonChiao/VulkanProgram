aebd7e9a9b9a47fbd635c3d98fe419f3e80facd5
JiaxiongJiao <jiaxiongjiao@icloud.com>
2022-01-28 17:19:43 -0500

    Changed <CMakeLists.txt> so that it can detect which platform CMake is being run

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,23 @@
 #include <vector>
 #include <cstdlib>
 
-class VulkanProgram {
+class VulkanProgram
+{
 public:
-    void run() {
+    void run()
+    {
         initVulkan();
     }
 
 private:
     VkBool32 vkResult{};
-    struct VulkanProgramInfo {
+    struct VulkanProgramInfo
+    {
         VkInstance vulkanInstance = VK_NULL_HANDLE;
-        VkPhysicalDevice GPU;
+        VkPhysicalDevice GPU = VK_NULL_HANDLE;
         std::vector<const char *> LayerEnabled =
                 {
-                    "VK_LAYER_KHRONOS_validation"
+                        "VK_LAYER_KHRONOS_validation"
                 };
 
         std::vector<const char *> InstanceExtensionsEnabled =
@@ -26,7 +29,8 @@ private:
 
     } vulkanProgramInfo;
 
-    void initVulkan() {
+    void initVulkan()
+    {
         VkInstanceCreateInfo instanceCreateInfo{};
         instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         instanceCreateInfo.enabledExtensionCount = vulkanProgramInfo.InstanceExtensionsEnabled.size();
@@ -39,12 +43,13 @@ private:
                                     nullptr,
                                     &vulkanProgramInfo.vulkanInstance);
 
-        if (vkResult != VK_SUCCESS) {
+        if (vkResult != VK_SUCCESS)
+        {
             std::cout << "Failed to create Vulkan instance" << std::endl;
             exit(-1);
         }
 
-        uint32_t  physicalDeviceCount = 0;
+        uint32_t physicalDeviceCount = 0;
         vkEnumeratePhysicalDevices(vulkanProgramInfo.vulkanInstance,
                                    &physicalDeviceCount,
                                    nullptr);
@@ -55,6 +60,12 @@ private:
                                    GPUs.data());
 
         vulkanProgramInfo.GPU = GPUs[0];
+        VkPhysicalDeviceProperties gpuProperties{};
+
+        vkGetPhysicalDeviceProperties(vulkanProgramInfo.GPU,
+                                      &gpuProperties);
+
+        std::cout << gpuProperties.deviceName << std::endl;
         VkFormatProperties formatProperties;
         vkGetPhysicalDeviceFormatProperties(vulkanProgramInfo.GPU,
                                             VK_FORMAT_R8G8B8A8_UNORM,
@@ -62,18 +73,45 @@ private:
 
         VkPhysicalDeviceMemoryProperties memoryProperties{};
         vkGetPhysicalDeviceMemoryProperties(vulkanProgramInfo.GPU, &memoryProperties);
-        std::cout << "Memory type count: " << memoryProperties.memoryTypeCount << std::endl;
-        std::cout << "Memory type heap index: " << memoryProperties.memoryTypes->heapIndex << std::endl;
-        std::cout << "Memory type property flag: " << std::bitset<32>(memoryProperties.memoryTypes->propertyFlags) << std::endl;
 
-        std::cout << "Memory heap count: " << memoryProperties.memoryHeapCount << std::endl;
-        std::cout << "Memory heap size: " << memoryProperties.memoryHeaps->size << std::endl;
-        std::cout << "Memory heap property flags: " << std::bitset<32>(memoryProperties.memoryHeaps->flags) << std::endl;
+        std::cout << "There are " << memoryProperties.memoryHeapCount << " heaps\n";
+        for (int currHeapIndex = 0; currHeapIndex < memoryProperties.memoryHeapCount; currHeapIndex++)
+        {
+            // Acquire information about current heap
+            std::cout << "  The heap at index " << currHeapIndex << " has the properties: \n";
+
+            std::cout << "      Memory heap size: "
+                      << (double) memoryProperties.memoryHeaps[currHeapIndex].size / (1024.0f * 1024.0f * 1024.0f)
+                      << std::endl;
+
+            std::cout << "      Memory heap property flags: "
+                      << std::bitset<32>(memoryProperties.memoryHeaps[currHeapIndex].flags)
+                      << std::endl;
+
+            // Print out this heap's memory type information
+            for (int currMemoryTypeIndex = 0;
+                 currMemoryTypeIndex < memoryProperties.memoryTypeCount; currMemoryTypeIndex++)
+            {
+                if (memoryProperties.memoryTypes[currMemoryTypeIndex].heapIndex == currHeapIndex)
+                {
+                    std::cout << "          the memory type that is corresponding to this heap is " << std::endl;
+                    std::cout << "          Memory type heap index: " << memoryProperties.memoryTypes[currMemoryTypeIndex].heapIndex
+                              << std::endl;
+                    std::cout << "          Memory type property flag: "
+                              << std::bitset<32>(memoryProperties.memoryTypes[currMemoryTypeIndex].propertyFlags) << std::endl;
+                }
+            }
+        }
+		std::cout << "Maximum number of color attachment: " << gpuProperties.limits.maxColorAttachments << std::endl;
+		std::cout << "Maximum framebuffer width: " << gpuProperties.limits.maxFramebufferWidth << std::endl;
+		std::cout << "Maximum frambuffer height: " << gpuProperties.limits.maxFramebufferHeight << std::endl;
+		std::cout << "Maximum frambuffer layer: " << gpuProperties.limits.maxFramebufferLayers << std::endl;
     }
 };
 
 
-int main() {
+int main()
+{
     VulkanProgram program{};
     program.run();
 }
