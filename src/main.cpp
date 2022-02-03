00e0ff71bd4b985891060a094f645b6df6eb70fd
JiaxiongJiao <jiaxiongjiao@icloud.com>
2022-02-02 23:17:36 -0500

    Added debug messenger to main program

--- src/main.cpp
+++ src/main.cpp
@@ -10,27 +10,81 @@ public:
     void run()
     {
         initVulkan();
+        createDebugMessenger();
         cleanup();
     }
 
 private:
     VkBool32 vkResult{};
+    VkDebugUtilsMessengerEXT debugMessenger = VK_NULL_HANDLE;
     struct VulkanProgramInfo
     {
         VkInstance vulkanInstance = VK_NULL_HANDLE;
 
         VkPhysicalDevice GPU = VK_NULL_HANDLE;
-		
+
         std::vector<const char *> LayerEnabled =
                 {
-                    "VK_LAYER_KHRONOS_validation",
+                        "VK_LAYER_KHRONOS_validation",
                 };
 
         std::vector<const char *> InstanceExtensionsEnabled =
                 {
+                        VK_EXT_DEBUG_UTILS_EXTENSION_NAME
                 };
 
     } vulkanProgramInfo;
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+            VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+            VkDebugUtilsMessageTypeFlagsEXT messageType,
+            const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+            [[maybe_unused]] void *pUserData
+    )
+    {
+        std::cout << "This is your customized debug callback for VkDebugUtilsMessenger\n";
+
+        if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT)
+        {
+            std::cout << "Message Severity: VERBOSE";
+        } else if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+        {
+            std::cout << "Message Severity: WARNING";
+        } else if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)
+        {
+            std::cout << "Message Severity: ERROR";
+        } else if (messageSeverity == VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT)
+        {
+            std::cout << "Message Severity: INFO";
+        } else
+        {
+            std::cout << "Message Severity: UNKNOWN";
+        }
+
+        std::cout << "\n";
+
+        if (messageType == VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT)
+        {
+            std::cout << "This is a GENERAL message";
+        } else if (messageType == VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT)
+        {
+            std::cout << "This is a VALIDATION message";
+        } else if (messageType == VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT)
+        {
+            std::cout << "This is a PERFORMANCE message";
+        } else
+        {
+            std::cout << "This is an UNKNOWN message";
+        }
+
+        std::cout << "\n";
+        std::cout << "Message ID Name: " << pCallbackData->pMessageIdName << std::endl;
+        std::cout << "Message ID Number: " << pCallbackData->messageIdNumber << std::endl;
+        std::cout << "Message is: " << pCallbackData->pMessage << std::endl;
+        std::cout << "== Finished ==\n\n\n" << std::endl;
+
+        return VK_FALSE;
+    }
 
     void initVulkan()
     {
@@ -42,6 +96,13 @@ private:
         instanceCreateInfo.ppEnabledLayerNames = vulkanProgramInfo.LayerEnabled.data();
         instanceCreateInfo.flags = 0;
 
+        // Fill in a debug messenger create info structure to put in instance create info
+        VkDebugUtilsMessengerCreateInfoEXT debugMessengerCreateInfo{};
+        fillInDebugMessengerCreateInfo(debugMessengerCreateInfo);
+
+        // After it is filled, put it in pNext of instance create info.
+        instanceCreateInfo.pNext = &debugMessengerCreateInfo;
+
         vkResult = vkCreateInstance(&instanceCreateInfo,
                                     nullptr,
                                     &vulkanProgramInfo.vulkanInstance);
@@ -51,24 +112,55 @@ private:
             std::cout << "Failed to create Vulkan instance" << std::endl;
             exit(-1);
         }
-
-        uint32_t physicalDeviceCount = 0;
-        vkEnumeratePhysicalDevices(vulkanProgramInfo.vulkanInstance,
-                                   &physicalDeviceCount,
-                                   nullptr);
-
-        std::vector<VkPhysicalDevice> GPUs(physicalDeviceCount);
-        vkEnumeratePhysicalDevices(vulkanProgramInfo.vulkanInstance,
-                                   &physicalDeviceCount,
-                                   GPUs.data());
-
-		// Choose which GPU to use
-        vulkanProgramInfo.GPU = GPUs[0];
     }
 
-    void cleanup()
+    void createDebugMessenger()
     {
+        // Find the function for creating debug messenger
+        auto createDebugUtilsMessenger = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+                vulkanProgramInfo.vulkanInstance,
+                "vkCreateDebugUtilsMessengerEXT");
+
+        VkDebugUtilsMessengerCreateInfoEXT debugMessengerCreateInfo{};
+        fillInDebugMessengerCreateInfo(debugMessengerCreateInfo);
+
+        createDebugUtilsMessenger(vulkanProgramInfo.vulkanInstance,
+                                  &debugMessengerCreateInfo,
+                                  nullptr,
+                                  &debugMessenger);
+    }
+
+    void cleanup() const
+    {
+        // Find function to destroy debug messenger
+        auto destroyDebugMessenger = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(
+                vulkanProgramInfo.vulkanInstance,
+                "vkDestroyDebugUtilsMessengerEXT"
+        );
+        destroyDebugMessenger(vulkanProgramInfo.vulkanInstance,
+                              debugMessenger,
+                              nullptr);
+
         vkDestroyInstance(vulkanProgramInfo.vulkanInstance, nullptr);
+
+
+
+    }
+
+    static void fillInDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo)
+    {
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.flags = 0;
+        createInfo.pNext = nullptr;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+                                     VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                                     VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT;
+
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT |
+                                 VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                                 VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT;
+
+        createInfo.pfnUserCallback = debugCallback;
     }
 };
 
