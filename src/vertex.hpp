eeabb9caba2af271de0352024f4618278535ad22
JiaxiongJiao <jiaxiongjiao@icloud.com>
2022-04-15 15:14:14 -0400

    Added vertex.hpp

---
+++ src/vertex.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Jiaxiong Jiao on 2022-04-14.
+//
+#include <glm/glm.hpp>
+
+#ifndef VULKANPROGRAM_VERTEX_HPP
+#define VULKANPROGRAM_VERTEX_HPP
+struct Vertex
+{
+    glm::vec2 pos;
+    glm::vec3 color;
+};
+
+const std::vector<struct Vertex> vertices =
+        {
+                {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+                {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+                {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+        };
+
+#endif //VULKANPROGRAM_VERTEX_HPP
